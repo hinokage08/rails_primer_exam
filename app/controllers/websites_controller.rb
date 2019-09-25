5e669402d771d8b892633113150e91a58d0206b6
youichi yamazaki <denncoru@yahoo.co.jp>
2019-09-25 17:20:20 +0900

    ツイート投稿画面を作成

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -1,4 +1,20 @@
 class WebsitesController < ApplicationController
+
   def index
+    @websites = Website.all
+  end
+
+  def new
+    @website = Website.new
+  end
+
+  def create
+    Website.create(website_params)
+    redirect_to new_website_path
+  end
+
+  private
+  def website_params
+    params.require(:website).permit(:content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get 'websites/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :websites
 end
